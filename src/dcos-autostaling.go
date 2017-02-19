37c8ff657f1a61762bdceaebe982ea3b1c95d966
Ahmad Posten <ahmad.hassan612@gmail.com>
2017-02-19 18:44:50 +0200

    Entry point

---
+++ src/dcos-autostaling.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"dcosautoscaling"
+	"sync"
+	"time"
+)
+
+func main() {
+	for {
+
+		allApplications, _ := dcosautoscaling.GetAll()
+		managedApplications := GetScalable(allApplications)
+
+		var wg sync.WaitGroup
+		wg.Add(len(managedApplications))
+
+		for _, application := range managedApplications {
+			go func() {
+				application.GetStatistics()
+				application.CalibrateDesired()
+				if application.Instances != application.Desired {
+					application.Adapt()
+				}
+			}()
+		}
+		wg.Wait()
+		time.Sleep(1 * time.Second)
+		// Through a go routing for everyone of them to check and scale
+	}
+}
+
+// Gets applications which are scalable
+func GetScalable(applications []dcosautoscaling.Application) []dcosautoscaling.Application {
+	var currentScalables []dcosautoscaling.Application
+	for _, application := range applications {
+		if application.IsScalable() {
+			application.SyncRules()
+			currentScalables = append(currentScalables, application)
+		}
+	}
+
+	// Remove the deleted apps
+	return currentScalables
+}
